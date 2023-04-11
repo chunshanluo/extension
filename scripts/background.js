aa2db5b0a88aa0633ed2cebcf4693a14f05098ce
eric <eric@163.com>
2023-04-11 09:06:26 +0800

    temporary stop

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,7 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     console.log(changeInfo)
     if (changeInfo.status == 'complete') {
+        console.log("complete")
         chrome.scripting.executeScript({
             target: {tabId: tabId, allFrames: true},
             files: ["scripts/content.js"]
